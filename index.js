34a4f158eb6089b362c7a5a3dcf468a9aeacc503
aartikaushik <aarti.kaushik04@gmail.com>
2018-06-08 16:29:13 +0530

    postgres database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var port = process.env.PORT || 5000
+var PORT = process.env.PORT || 5000
 
 
 
@@ -10,4 +10,29 @@ app.get('/', function(req, res){
 
 });
 
-app.listen(port);
+app.listen(PORT, function(){
+console.log("listening");
+});
+
+
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    res.render('pages/db', result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+
+

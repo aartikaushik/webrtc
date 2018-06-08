7482808317998276854809715c74ec491c152142
aartikaushik <aarti.kaushik04@gmail.com>
2018-06-08 19:29:18 +0530

    error trial

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/db', async (req, res) => {
   try {
     const client = await pool.connect()
     const result = await client.query('SELECT * FROM test_table');
-    res.render('pages/db', result);
+    res.render('/db', result);
     client.release();
   } catch (err) {
     console.error(err);

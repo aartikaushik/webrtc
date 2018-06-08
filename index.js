0d1f27383def7f0f7ed02b9bf3875794a0958386
aartikaushik <aarti.kaushik04@gmail.com>
2018-06-08 20:38:46 +0530

    query result

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/db', async (req, res) => {
   try {
     const client = await pool.connect()
     const result = await client.query('SELECT * FROM test_table');
-    res.json(result);
+    res.send(result.rows);
     client.release();
   } catch (err) {
     console.error(err);

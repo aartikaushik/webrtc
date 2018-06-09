6f076ace3782a727821bf054d287b2e5473ba7e0
aartikaushik <aarti.kaushik04@gmail.com>
2018-06-09 17:01:49 +0530

    jkssdhj

--- index.js
+++ index.js
@@ -37,14 +37,14 @@ socket.on('chat message', function(msg){
 });
 var pg = require('pg');
 
+
 var connectionString = "postgres://bymbnvfyrosgvz:1aee5875cdbedd2d8c4fbbb9c1f9b44088f2b36111f9c78090cbf58986e9f8e1@ec2-54-225-107-174.compute-1.amazonaws.com:5432/d3gvoum1kvv887"
- 
+
 pg.connect(connectionString, function(err, client, done) {
-   client.query('SELECT * FROM provide_connection', function(err, result) {
+   client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if(err) return console.error(err);
       console.log(result.rows);
    });
 });
-
 

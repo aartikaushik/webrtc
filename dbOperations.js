78d1b6fa44327436b3856eae00d919ca1bf15d42
aartikaushik <aarti.kaushik04@gmail.com>
2018-06-09 18:06:58 +0530

    jkssdhj

--- dbOperations.js
+++ dbOperations.js
@@ -4,23 +4,12 @@ module.exports = {
       
         //You can run command "heroku config" to see what is Database URL from Heroku belt
       
-        var conString = process.env.DATABASE_URL || "postgres://postgres:Welcome123@localhost:5432/postgres";
+        var conString = process.env.DATABASE_URL || "postgres://bymbnvfyrosgvz:1aee5875cdbedd2d8c4fbbb9c1f9b44088f2b36111f9c78090cbf58986e9f8e1@ec2-54-225-107-174.compute-1.amazonaws.com:5432/d3gvoum1kvv887";
         var client = new pg.Client(conString);
 
         client.connect();
 
-        var query = client.query("select * from provide_connection");
-
-        query.on("row", function (row, result) { 
-            result.addRow(row); 
-        });
-
-        query.on("end", function (result) {          
-            client.end();
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.write(JSON.stringify(result.rows, null, "    ") + "\n");
-            res.end();  
-        });
+     
   },
     
 

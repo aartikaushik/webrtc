78eea78b85852570249f19c33087c593647ce9e8
aartikaushik <aarti.kaushik04@gmail.com>
2018-06-09 18:25:16 +0530

    jkssdhj

--- dbOperations.js
+++ dbOperations.js
@@ -1,21 +1,24 @@
 module.exports = {
-  getRecords: function(req, res) {    
-        var pg = require('pg');  
-      
-        //You can run command "heroku config" to see what is Database URL from Heroku belt
-      
-        var conString = process.env.DATABASE_URL || "postgres://bymbnvfyrosgvz:1aee5875cdbedd2d8c4fbbb9c1f9b44088f2b36111f9c78090cbf58986e9f8e1@ec2-54-225-107-174.compute-1.amazonaws.com:5432/d3gvoum1kvv887";
-        var client = new pg.Client(conString);
-        try{
-        client.connect();
+  getRecords: function(req, res) { 
 
-        var query = client.query("select * from provide_connection");
-        res.json(query);
-        client.release();
-        } catch (err) {
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});   
+       
+      
+ 
+       try {
+    const client = await pool.connect()
+    const result = await client.query('select * from test_table');
+    res.send(result.rows);
+    client.release();
+  } catch (err) {
     console.error(err);
     res.send("Error " + err);
   }
+
   },
     
 

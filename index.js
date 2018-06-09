3a141973f3220d2bd747232c854483b0d3699028
aartikaushik <aarti.kaushik04@gmail.com>
2018-06-09 15:39:21 +0530

    shortid generation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io').listen(http);
+var shortid = require('short-id');
+ 
 
 var PORT = process.env.PORT || 5000
 
@@ -17,10 +19,21 @@ console.log("listening");
 });
 
 
-
-
 io.on('connection', function(socket){
+  
   console.log('a user connected');
+
+socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    var unique_id = shortid.generate()
+    console.log(unique_id);
+    socket.emit('id', unique_id);
+    
+  });
+
 });
 
 
@@ -45,4 +58,3 @@ app.get('/db', async (req, res) => {
     res.send("Error " + err);
   }
 });
-

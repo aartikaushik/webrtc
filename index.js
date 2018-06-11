08c2ed713c48273a63a96dad5d4b559755af2424
aartikaushik <aarti.kaushik04@gmail.com>
2018-06-11 21:09:54 +0530

    showstringddc

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ http.listen(PORT, function(){
 });
 
 app.use(express.static('public'));
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
+
+	socket.on('chat message', function(msg){
+		console.log('message: ' + msg);
+		var unique_id = shortid.generate()
+		console.log(unique_id);
+		socket.emit('id', unique_id);
+		db_insert(msg,unique_id)
+	});
+
+});
+
 const { Pool } = require('pg');
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
@@ -30,6 +48,19 @@ function db_insert(msg,unique_id) {
 		const result = await client.query('SELECT * FROM provide_connection');
 		res.send(result.rows)
 		client.release();
+
+	app.get('/:unique_id', function(req,res){
+	
+
+		const result = await client.query('SELECT * FROM provide_connection where url_id = req.params.unique_id');
+		res.send(result.rows)
+		client.release();
+	
+	
+
+	});
+
+
 	} 
 	catch (err) {
 		console.error(err);
@@ -38,39 +69,6 @@ function db_insert(msg,unique_id) {
 	});
 }
 
-io.on('connection', function(socket){
-	console.log('a user connected');
-
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-	});
-
-	socket.on('chat message', function(msg){
-		console.log('message: ' + msg);
-		var unique_id = shortid.generate()
-		console.log(unique_id);
-		socket.emit('id', unique_id);
-		db_insert(msg,unique_id)
-	});
-
-});
-
-
-
-
-app.get('/:unique_id', function(req,res){
-	try {
-
-		const result = client.query('SELECT * FROM provide_connection where url_id = req.params.unique_id');
-		res.send(result.rows)
-		client.release();
-	}
-	catch (err) {
-		console.error(err);
-		res.send("Error " + err);
-	}
-
-});
 
 
 

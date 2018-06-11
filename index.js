746fb44745d37c4357fd15cdce1437b687f83ff6
aartikaushik <aarti.kaushik04@gmail.com>
2018-06-11 18:50:27 +0530

    showstringddc

--- index.js
+++ index.js
@@ -70,10 +70,10 @@ function db_insert(msg,unique_id) {
 
 app.get('/:unique_id', function(req,res){
 try {
-const client1 = pool.connect()
-const result1 = client1.query('SELECT * FROM provide_connection');
-	    res.send(result1.rows)
-	    client1.release();
+
+const result = client.query('SELECT * FROM provide_connection');
+	    res.send(result.rows)
+	    client.release();
 }catch (err) {
 console.error(err);
 res.send("Error " + err);

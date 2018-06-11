717be6d3ce27236edc917545dcdd0ed6a8a7fb25
aartikaushik <aarti.kaushik04@gmail.com>
2018-06-11 18:30:36 +0530

    showstringddc

--- index.js
+++ index.js
@@ -65,8 +65,7 @@ function db_insert(msg,unique_id) {
 app.get('/:unique_id', function(req,res){
 const client1 = pool.connect()
 const result1 = client1.query('SELECT * FROM provide_connection');
-	    res.send(result1.rows)
-	    client1.release();
+	  
 res.send(req.params.unique_id)
 
 });

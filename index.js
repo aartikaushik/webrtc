282bab0b5009871612b522827ab2b88ccaecb317
aartikaushik <aarti.kaushik04@gmail.com>
2018-06-12 12:45:06 +0530

    showstr

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ app.get('/:uni_id', async (req, res) => {
 		const client = await pool.connect()
                 client.query("INSERT INTO provide_connection values($1, $2, $3)", [1, msg, unique_id]);
 		const result = await client.query('SELECT request_string FROM provide_connection where url_id = $1', [req.params.uni_id]);
-		res.json(result)
 		var user = result[0].request_string;
 		res.send(user)
 		client.release();

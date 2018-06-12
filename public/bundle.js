e0e9302d141be6d265008d47dd34b4c768bddd72
aartikaushik <aarti.kaushik04@gmail.com>
2018-06-12 15:51:55 +0530

    showstr

--- public/bundle.js
+++ public/bundle.js
@@ -16,11 +16,10 @@ p.on('signal', function (data) {
 })
 
 socket.on('req_str', function(req_str){
-document.getElementById("incoming").innerHTML = req_str; 
-     document.querySelector('form').addEventListener('submit', function (ev) {
-  ev.preventDefault()
-  p.signal(JSON.parse(document.querySelector('#incoming').value))
-})
+
+    
+  p.signal(JSON.parse(req_str))
+
     });
     
 

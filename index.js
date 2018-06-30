3b1b1281a163eccafe1bd3dedfd08d3c1e6e90c7
aartikaushik <aarti.kaushik04@gmail.com>
2018-06-30 23:07:16 +0530

    remove image try

--- index.js
+++ index.js
@@ -1,75 +1,105 @@
-var express = require('express')
-var app= express()
-var http = require('http').createServer(app)
-var io = require('socket.io').listen(http)
-var shortid = require('short-id')
- 
-var PORT = process.env.PORT || 5000
+// including express
+var express = require('express');
+var app= express();
 
+// creating server
+var http = require('http').createServer(app);
+
+// including socket.io and shortid
+var io = require('socket.io').listen(http);
+var shortid = require('short-id');
+
+// dynamic port on heroku or 5000 on localhost
+var PORT = process.env.PORT || 5000;
+
+// server listening for the client to connect
 http.listen(PORT, function(){
-	console.log("listening")
+	console.log("listening");
 })
 
-app.use(express.static('public'))
+// rendering index.html from public folder as the home page
+app.use(express.static('public'));
 
-var data=[]
+// for keeping track of initiator and receiver socket
+var data=[];
+
+// on socket connection between server and client
 io.on('connection', function(socket){
-	console.log('a user connected')
+	console.log('a user connected');
 
 	socket.on('disconnect', function(){
-		console.log('user disconnected')
+		console.log('user disconnected');
 	})
+
+	// offer(request string) sent by initiator is catched here
 	socket.on('request string', function(msg){
-                if(msg.role == 'initiator')
+        if(msg.role == 'initiator')
 		{
-			var unique_id = shortid.generate()
-		        data.push({"initiator_text": msg , "url_id": unique_id , "initiator_socket": socket , "receiver_text": "" , "receiver_socket": "" })
-  			socket.emit('id', unique_id)
+			var unique_id = shortid.generate();
+		    data.push({"initiator_text": msg , "url_id": unique_id , "initiator_socket": socket , "receiver_text": "" , "receiver_socket": "" });
+  			socket.emit('id', unique_id);
 		}		
 	})
+
+	// role and unique_id sent by receiver as initial greeting
 	socket.on('receiver welcome', function(greeting){
 		if(greeting.role == 'receiver')
 		{
 			var data_len = data.length;
-			for (var i = 0; i < data_len; i++) {
-                             if(data[i] != undefined) {
+			for (var i = 0; i < data_len; i++) 
+			{
+				if(data[i] != undefined) 
+				{
 					if(data[i].url_id == greeting.url_id)
 					{
-						data[i].receiver_socket = socket
-						var rec_soc = data[i].receiver_socket
-						var ini_text = data[i].initiator_text.req_str
-						break
+						data[i].receiver_socket = socket;
+						var rec_soc = data[i].receiver_socket;
+						var ini_text = data[i].initiator_text.req_str;
+						break;
 					}  
-			     }
+			    }
 			}
-			rec_soc.emit('initiator text', ini_text)
+
+			// send initiator text onto receiver socket
+			rec_soc.emit('initiator text', ini_text);
 		}
 	})
+
+	// on receiving answer(reply) of offer(initiator text/ request string) from receiver side is catched here
 	socket.on('receiver answer', function(answer){
 		var data_len = data.length;
-			for (var i = 0; i < data_len; i++) {
-                            if(data[i] != undefined){
+		for (var i = 0; i < data_len; i++) 
+		{
+			if(data[i] != undefined)
+			{
 				if(data[i].url_id == answer.url_id)
 				{
-					data[i].receiver_text = answer.receiver_text
-					var ini_soc = data[i].initiator_socket
-					break
+					data[i].receiver_text = answer.receiver_text;
+					var ini_soc = data[i].initiator_socket;
+					break;
  				} 
-			     }
 			}
-		ini_soc.emit('receiver answer', answer.receiver_text)
-		for (var i = 0; i < data_len; i++) {
-                        if(data[i] != undefined){  
+		}
+
+		// sending receiver answer on initiator socket
+		ini_soc.emit('receiver answer', answer.receiver_text);
+
+		// deleting the entry so as to cut of signaling server
+		for (var i = 0; i < data_len; i++) 
+		{
+			if(data[i] != undefined)
+			{  
 				if(data[i].url_id == answer.url_id)
 				{
-					delete data[i]
-					break
+					delete data[i]; // deleting entry will make that index undefined
+					break;
  				}
-                         }  
-	        }
+            }  
+	    }
 	})
 })
 
+// rendering index.html for receiver hitting the url having unique id
 app.get('/:uni_id', function(req, res){
 	res.sendFile( __dirname + "/public/" + "index.html" )
 })
--- public/bundle.js
+++ public/bundle.js
@@ -1,126 +1,107 @@
-var socket = io()
-var Peer = SimplePeer
-var lastPart = window.location.href.split("/").pop()
+var socket = io();
+var Peer = SimplePeer;
+
+// for grabbing last unique id in the url
+var lastPart = window.location.href.split("/").pop();
+
+// receiver
 if(lastPart != '')
 {
-	var p = new Peer({ initiator: false , trickle: false })
-        var greeting = {"role": 'receiver', "url_id": lastPart} 
-        socket.emit('receiver welcome', greeting)
+	// initializing constructor for peer
+	var p = new Peer({ initiator: false , trickle: false });
+
+	// sending role and url id as greeting message to server in response to which server will send initiator text
+    var greeting = {"role": 'receiver', "url_id": lastPart};
+	socket.emit('receiver welcome', greeting);
+	
+	// initiator text from the server side is catched here
 	socket.on('initiator text', function(str){	
-		p.on('signal', function (data) {
-			console.log('SIGNAL', JSON.stringify(data))
-			var receiver_answer = {"role": 'receiver', "receiver_text": JSON.stringify(data), "url_id": lastPart} 
-			socket.emit('receiver answer', receiver_answer)
+		p.on('signal', function (data){
+			//console.log('SIGNAL', JSON.stringify(data));
+
+			//sending answer(reply) back to server
+			var receiver_answer = {"role": 'receiver', "receiver_text": JSON.stringify(data), "url_id": lastPart};
+			socket.emit('receiver answer', receiver_answer);
 		})
-		p.on('error', function (err) { console.log('error', err) })
-		p.signal(JSON.parse(str))		
+		p.on('error', function (err) { console.log('error', err) });
+		p.signal(JSON.parse(str));		
 	})
-        var text_div = document.createElement("div")
-	text_div.className = 'share_url'
-	text_div.id = 'share_url'
-	document.getElementById("side_pannel").appendChild(text_div)	
+    var link_div = document.createElement("div");
+	link_div.id = 'share_url';
+	document.getElementById("side_pannel").appendChild(link_div);	
 }
+
+// initiator
 else
 {
-	var p = new Peer({ initiator: true , trickle: false })
+	//  initializing constructor
+	var p = new Peer({ initiator: true , trickle: false });
+
 	p.on('signal', function (data) {
-		console.log('SIGNAL', JSON.stringify(data))
-		var role_and_string = {"role": 'initiator', "req_str": JSON.stringify(data)} 
-		socket.emit('request string', role_and_string)
+		//console.log('SIGNAL', JSON.stringify(data));
+
+		// sending role and request string to server
+		var role_and_string = {"role": 'initiator', "req_str": JSON.stringify(data)};
+		socket.emit('request string', role_and_string);
+
+		// unique id sent by server is catched here
 		socket.on('id', function(id){
-		    var text_div = document.createElement("div")
-		    text_div.className = 'share_url'
-		    text_div.id = 'share_url'
-		    text_div.textContent = "Share this URL with whom you want to chat : radiant-basin-26448.herokuapp.com/" + id 
-		    document.getElementById("side_pannel").appendChild(text_div) 
+		    var link_div = document.createElement("div");
+		    link_div.id = 'share_url';
+		    link_div.textContent = "Share this URL with whom you want to chat : radiant-basin-26448.herokuapp.com/" + id ;
+		    document.getElementById("side_pannel").appendChild(link_div);
 		})   
 	})
-	p.on('error', function (err) { console.log('error', err) })
+	p.on('error', function (err) { console.log('error', err) });
+
+	// receiver answer sent by server is catched here
 	socket.on('receiver answer', function(ans){
-		p.signal(JSON.parse(ans))
+		p.signal(JSON.parse(ans));
 	})
 }
      
 p.on('connect', function () {
-	console.log('CONNECT')
-	document.getElementById("share_url").textContent = "Connected"
+	//console.log('CONNECT');
+	document.getElementById("share_url").textContent = "Connected";
+
+	// on click of send button this will execute
 	document.getElementById("send").addEventListener("click", function () {
-		if((document.getElementById('message').value == null || document.getElementById('message').value == '') && document.getElementById('pic').value == '')
+		
+		if(document.getElementById('message').value != null || document.getElementById('message').value != '')
 		{
-			var data_and_type = {"type": 'blank' , "info": ''}	
-		}
-		else if((document.getElementById('message').value != null || document.getElementById('message').value != '') && document.getElementById('pic').value == '')
-		{
-			var data_and_type = {"type": 'text' , "info": document.querySelector('#message').value}
-			p.send(JSON.stringify(data_and_type))		  
-			var list = document.createElement("li")
-			list.id = 'list'
-			var span = document.createElement("span")
-			span.className = 'right'
-			var new_div = document.createElement("div")
-			new_div.className = 'clear'
-			span.textContent = document.querySelector('#message').value
-			list.appendChild(span) 
-			list.appendChild(new_div) 
-			document.getElementById("ulist").appendChild(list)		
-			document.getElementById("message").value = ''
-		}
-		if(document.getElementById('pic').value != '')
-		{	
-			var data_and_type = {"type": 'image' , "info": document.getElementById("base64_string").value } 
-			p.send(JSON.stringify(data_and_type))
-			var list = document.createElement("li")
-			list.id = 'list'
-			var span = document.createElement("span")
-			span.className = 'right'
-			var image = document.createElement("img")
-			image.src = data_and_type.info
-			var new_div = document.createElement("div")
-			new_div.className = 'clear'
-			span.appendChild(image) 
-			list.appendChild(span) 
-			list.appendChild(new_div)
-			document.getElementById("ulist").appendChild(list)		
-			document.getElementById("pic").value = ''
-	
-		}
-			
+			p.send(document.querySelector('#message').value);		  
+			var list = document.createElement("li");
+			list.id = 'list';
+			var span = document.createElement("span");
+			span.className = 'right';
+			var new_div = document.createElement("div");
+			new_div.className = 'clear';
+			span.textContent = document.querySelector('#message').value;
+			list.appendChild(span);
+			list.appendChild(new_div); 
+			document.getElementById("ulist").appendChild(list);		
+			document.getElementById("message").value = '';
+			var myDiv = document.getElementById("chat-window");
+			myDiv.scrollTop = myDiv.scrollHeight;
+		}		
 	})
 })
 
 p.on('data', function (data) {
-	var obj = JSON.parse(data)
-	if(obj.type == 'text')
-	{
-		var list = document.createElement("li")
-		list.id = 'list'
-		var span = document.createElement("span")
-		span.className = 'left'
-		var new_div = document.createElement("div")
-		new_div.className = 'clear'
-		span.textContent = obj.info
-		list.appendChild(span) 
-		list.appendChild(new_div)
-		document.getElementById("ulist").appendChild(list)
-		console.log('data: ' + data)
-	}
-	else if(obj.type == 'image')
-	{
-		console.log(obj)
-		var list = document.createElement("li")
-		list.id = 'list'
-		var span = document.createElement("span")
-		span.className = 'left'
-		var image = document.createElement("img")
-		image.src = obj.info
-		var new_div = document.createElement("div")
-		new_div.className = 'clear'
-		span.appendChild(image) 
-		list.appendChild(span) 
-		list.appendChild(new_div)
-		document.getElementById("ulist").appendChild(list)
-		
-	}
+
+		var list = document.createElement("li");
+		list.id = 'list';
+		var span = document.createElement("span");
+		span.className = 'left';
+		var new_div = document.createElement("div");
+		new_div.className = 'clear';
+		span.textContent = data;
+		list.appendChild(span);
+		list.appendChild(new_div);
+		document.getElementById("ulist").appendChild(list);
+		var myDiv = document.getElementById("chat-window");
+		myDiv.scrollTop = myDiv.scrollHeight;
+		//console.log('data: ' + data)
 })
 
 
